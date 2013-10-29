a46c0a6023fb8523c459254fa0ae9ab68a012fbb
Xiao Liu <xiaoliu@email.arizona.edu>
2013-10-29 00:16:13 -0700

    update grav.py to suppor flexible file sizes and merge.py for new format of output

--- gravity-model.sh
+++ gravity-model.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #download project files
-git clone git://github.com/vulix/gravity.git work-queue
+wget https://github.com/vulix/gravity/archive/master.zip
+unzip master
 #download data files
 wget 'http://de.iplantcollaborative.org/dl/d/0e0ab561-8281-4c6f-aa1e-1e4b4b35125e/gravity_data_ACIC_JKennedy.tgz'
 tar -zxvf gravity_data_ACIC_JKennedy.tgz
@@ -11,7 +12,7 @@ split -a4 -l $2 test1_grav_pos.txt grav_pos/pos
 #set environmental variables
 export PATH=~/cctools/bin:${PATH}
 export PYTHONPATH=${PYTHONPATH}:~/cctools/lib/python2.6/site-packages
-cd work-queue
+cd gravity-master
 python wq.py
 python merge.py $3 $4
 rm grav*.txt
